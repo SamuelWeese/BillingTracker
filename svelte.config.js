1c7364f1bf128736842ca90cd49465b57a90898f
Samuel Weese <weesesr@mail.uc.edu>
2024-10-01 23:46:01 -0400

    added direct route to github.io

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import adapter from '@sveltejs/adapter-node';
 
 export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? 'https://SamuelWeese.github.io/BillingTracker' : '';
+        }
 	}
 };
